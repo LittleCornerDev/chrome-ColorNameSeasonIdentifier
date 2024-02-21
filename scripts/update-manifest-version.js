e202a932801c548940e9191c151656539e85669a
LittleCornerDev <7387317+LittleCornerDev@users.noreply.github.com>
2024-02-20 20:06:39 -0500

    build(vcs): add manifest version update script

---
+++ scripts/update-manifest-version.js
@@ -0,0 +1,16 @@
+const pjson = require("../package.json");
+const version = pjson.version;
+console.log(`Detected version ${version} from package.json`);
+
+const editJsonFile = require("edit-json-file");
+
+// Load manifest.json
+// NOTE: If the file doesn't exist, the content will be an empty object by default.
+let file = editJsonFile(`source/manifest.json`);
+
+// Set manifest.json version to same as package.json
+file.set("version", version);
+console.log(`Updated manifest.json version to ${version}.`);
+
+// Save the data to the disk
+file.save();
