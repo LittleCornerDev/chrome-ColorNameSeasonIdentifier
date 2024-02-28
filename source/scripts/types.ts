7614b7e5f1a3523844c0840fcd45e6c2ef293ed6
LittleCornerDev <7387317+LittleCornerDev@users.noreply.github.com>
2024-02-27 21:15:29 -0500

    build(*): fix prettier
    rename config file, add md override, skip hbs

--- source/scripts/types.ts
+++ source/scripts/types.ts
@@ -1,40 +1,40 @@
 export type ColorDataSource = "W3C" | "X11" | "Crayola" | "RAL" | "Pantone";
 export type ColorDataType = "names" | "seasons";
 export type ColorDataTypeNames = ColorMatch & {
-  names?: ColorNameInfo[];
+	names?: ColorNameInfo[];
 };
 export type ColorDataTypeSeasons = ColorMatch & {
-  good?: ColorSeasonInfo[];
-  bad?: ColorSeasonInfo[];
+	good?: ColorSeasonInfo[];
+	bad?: ColorSeasonInfo[];
 };
 export type ColorMatch = {
-  hex?: ColorValueHexWithHash;
-  isExactMatch?: boolean;
+	hex?: ColorValueHexWithHash;
+	isExactMatch?: boolean;
 };
 export type ColorNameInfo = {
-  name: string;
-  source: ColorDataSource;
-  number?: string;
+	name: string;
+	source: ColorDataSource;
+	number?: string;
 };
 export type ColorSeasonInfo = {
-  season: Season;
-  seasonType: SeasonType;
-  colorType?: ColorType;
+	season: Season;
+	seasonType: SeasonType;
+	colorType?: ColorType;
 };
 export type ColorType = "fn" | "ca" | "m";
 export type ColorValueHex = ColorValueHexWithHash | ColorValueHexNoHash;
 export type ColorValueHexNoHash = string;
 export type ColorValueHexWithHash = `#${ColorValueHexNoHash}`;
 export type ColorData = Record<
-  ColorValueHexNoHash,
-  {
-    names?: ColorDataTypeNames;
-    seasons?: ColorDataTypeSeasons;
-    rgb?: RgbData;
-    hcl?: HclData;
-    hsl?: HslData;
-    hsv?: HsvData;
-  }
+	ColorValueHexNoHash,
+	{
+		names?: ColorDataTypeNames;
+		seasons?: ColorDataTypeSeasons;
+		rgb?: RgbData;
+		hcl?: HclData;
+		hsl?: HslData;
+		hsv?: HsvData;
+	}
 >;
 
 export type Decimal = number;
@@ -44,19 +44,19 @@ export type Hexadecimal = `0x${number}`;
 export type HexString = `0x${string}`;
 
 export type HclData = {
-  h: Degrees;
-  c: number;
-  l: Percent;
+	h: Degrees;
+	c: number;
+	l: Percent;
 };
 export type HslData = {
-  h: Degrees;
-  s: Percent;
-  l: Percent;
+	h: Degrees;
+	s: Percent;
+	l: Percent;
 };
 export type HsvData = {
-  h: Degrees;
-  s: Percent;
-  v: Percent;
+	h: Degrees;
+	s: Percent;
+	v: Percent;
 };
 /*export enum HsvType {
 	H = 'h',
@@ -84,10 +84,10 @@ export type RgbType = "r" | "g" | "b" | "a";
 
 export type Season = "winter" | "spring" | "summer" | "autumn";
 export type SeasonType =
-  | SeasonTypeWinter
-  | SeasonTypeSpring
-  | SeasonTypeSummer
-  | SeasonTypeAutumn;
+	| SeasonTypeWinter
+	| SeasonTypeSpring
+	| SeasonTypeSummer
+	| SeasonTypeAutumn;
 export type SeasonTypeAutumn = "dark" | "true" | "soft";
 export type SeasonTypeSpring = "light" | "true" | "bright";
 export type SeasonTypeSummer = "light" | "true" | "soft";
@@ -95,9 +95,9 @@ export type SeasonTypeWinter = "dark" | "true" | "bright";
 
 export type IdentifierState = "closed" | "opened";
 export type StorageData = {
-  identifier: IdentifierState;
-  port: chrome.runtime.Port;
-  screenshot: string;
+	identifier: IdentifierState;
+	port: chrome.runtime.Port;
+	screenshot: string;
 };
 export type StorageDataKey = "identifier" | "port" | "screenshot";
 //export type StorageDataValue = StorageData[ StorageDataKey ];
@@ -106,39 +106,39 @@ export type StorageDataValue = IdentifierState | chrome.runtime.Port | string;
 // Use array of string ids as object keys
 // https://codyarose.com/blog/object-keys-from-array-in-typescript/
 export const styleClassNames = [
-  "main",
-  "origin",
-  "originHex",
-  "originRgba",
-  "originRgbaR",
-  "originRgbaG",
-  "originRgbaB",
-  "originRgbaA",
-  "originHcl",
-  "originHclH",
-  "originHclC",
-  "originHclL",
-  "originHsl",
-  "originHslH",
-  "originHslS",
-  "originHslL",
-  "originHsv",
-  "originHsvH",
-  "originHsvS",
-  "originHsvV",
-  "name",
-  "nameHex",
-  "nameNames",
-  "nameExact",
-  "seasons",
-  "seasonsHex",
-  "seasonsGood",
-  "seasonsBad",
-  "seasonsExact",
+	"main",
+	"origin",
+	"originHex",
+	"originRgba",
+	"originRgbaR",
+	"originRgbaG",
+	"originRgbaB",
+	"originRgbaA",
+	"originHcl",
+	"originHclH",
+	"originHclC",
+	"originHclL",
+	"originHsl",
+	"originHslH",
+	"originHslS",
+	"originHslL",
+	"originHsv",
+	"originHsvH",
+	"originHsvS",
+	"originHsvV",
+	"name",
+	"nameHex",
+	"nameNames",
+	"nameExact",
+	"seasons",
+	"seasonsHex",
+	"seasonsGood",
+	"seasonsBad",
+	"seasonsExact",
 ] as const;
 export type StyleClassNameKey = (typeof styleClassNames)[number];
 export type StyleClassNames = {
-  [K in StyleClassNameKey]: string;
+	[K in StyleClassNameKey]: string;
 };
 
 export type TabId = number;
